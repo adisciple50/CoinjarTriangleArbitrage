320ea04faf7cb8b8c36c5590cc03b0b4a27c9c57
Jason Crockett <JesusisLord33333@outlook.com>
2023-12-15 08:23:16 +0000

    Initial commit

---
+++ lib/CoinjarTriangleArbitrage.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "CoinjarTriangleArbitrage/version"
+
+module CoinjarTriangleArbitrage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/CoinjarTriangleArbitrage/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CoinjarTriangleArbitrage
+  VERSION = "0.1.0"
+end
